761f87015d67bee4cb5bfd91591dd3431858adbb
dome2073 <wlgns2073@gmail.com>
2020-09-21 18:37:30 +0900

    자바 1000제

---
+++ Java1000/src/WordScramble/WordScrambleEx1.java
@@ -0,0 +1,36 @@
+package WordScramble;
+
+class WordScrambleEx1 {
+    public static void main(String[] args) {
+          String[] strArr = { "CHANGE", "LOVE", "HOPE", "VIEW"};
+
+          String answer = getAnswer(strArr);
+          String question = getScrambledWord(answer);
+
+          System.out.println("Question:"+question);
+          System.out.println("Answer:"+answer);
+    } // main
+
+    public static String getAnswer(String[] strArr) { 
+    	
+    	//배열 strArr의 요소중 하나를 임의로 골라서 반환
+    	//임의의 수 0~ 배열의 크기 만큼
+    	int random = (int) (Math.random() * strArr.length);
+    	
+    	return strArr[random];
+    }
+   
+    public static String getScrambledWord(String str) { 
+    	
+    	char[] tmp = null;
+    	System.out.println(str.length());
+    	
+    	//1.주어진 문자열 str의 각 문자의 순서를 섞는다 
+    	for(int i=0; i<str.length(); i++) {
+    		
+    	}
+    	//2.새로운 문자열로 반환한다.
+    	
+    	return "";
+    } // scramble(String str)
+}
